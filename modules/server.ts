0ee2249c25338c21483e8885a89290a145eb19f5
Aditya Khot <adityakhot6838@gmail.com>
2023-11-25 10:21:40 +0530

    Update Supabase URL in server.ts

--- modules/server.ts
+++ modules/server.ts
@@ -3,23 +3,22 @@ import { environment } from "@zuplo/runtime";
 import { createClient } from "@supabase/supabase-js"
 
 const sb = () => {
-  const supabaseUrl = 'https://vvwurcluclmfbskwmkqy.supabase.co';
+  const supabaseUrl = 'https://rbbiufqawlwcslpdmmnz.supabase.co';
   const supabaseAnonKey = environment.SUPABASE_SERVICE_KEY!
   const supabase = createClient(supabaseUrl, supabaseAnonKey);
   return supabase;
 }
-
 
 export async function getAllThemes(request: ZuploRequest, context: ZuploContext) {
 
   const supabase = sb();
 
   const { data: countries, error } = await supabase
-    .from('countries')
+    .from('themes')
     .select('*')
 
   if (error) {
-    throw new Error("something went wrong")
+    throw new Error(JSON.stringify(error))
   }
 
   return countries
